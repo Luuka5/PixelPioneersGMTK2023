2fd9b0e5eb86616c6660f41b3ecca096d09aadd8
getraid <32432718+getraid@users.noreply.github.com>
2023-07-08 22:06:47 +0200

    door asset new

--- Assets/Scripts/Lever.cs
+++ Assets/Scripts/Lever.cs
@@ -8,18 +8,37 @@ public class Lever : MonoBehaviour
    public List<Door> ConnectedDoors;
     private bool interacted = false;
 
+    public GameObject Untoggled;
+    public GameObject Toggled;
+
+    public bool isToggled = false;
+
+    
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        Debug.Log("Trigger: " + interacted);
+       
         if (Input.GetButtonDown("Interact"))
         {
-            if(interacted == false)
+            if (interacted == false)
             {
                 foreach (var item in ConnectedDoors)
                 {
                     item.ChangeDoor();
                 }
                 interacted = true;
+
+                isToggled = !isToggled;
+                if (isToggled)
+                {
+                    Untoggled.SetActive(false);
+                    Toggled.SetActive(true);
+                }
+                else 
+                    {
+                    Untoggled.SetActive(true);
+                    Toggled.SetActive(false);
+                }
             }
             
         }
